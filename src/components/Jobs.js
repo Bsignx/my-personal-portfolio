c71e4f47c2e117eaeead85b3683cc0973866bbf2
Bruno Mariano <bsignx@gmail.com>
2020-09-01 17:17:29 -0300

    style: styled jobs section

--- src/components/Jobs.js
+++ src/components/Jobs.js
@@ -1,11 +1,66 @@
-import React from "react"
-import Title from "./Title"
-import { FaAngleDoubleRight } from "react-icons/fa"
-import { graphql, useStaticQuery } from "gatsby"
-import { Link } from "gatsby"
+import React, { useState } from 'react';
+import Title from './Title';
+import { FaAngleDoubleRight } from 'react-icons/fa';
+import { graphql, useStaticQuery } from 'gatsby';
+import { Link } from 'gatsby';
+
+const query = graphql`
+  {
+    allStrapiJobs(sort: { fields: strapiId, order: DESC }) {
+      nodes {
+        company
+        position
+        date
+        description {
+          id
+          name
+        }
+      }
+    }
+  }
+`;
 
 const Jobs = () => {
-  return <h2>jobs component</h2>
-}
+  const data = useStaticQuery(query);
+  const {
+    allStrapiJobs: { nodes: jobs },
+  } = data;
+  const [value, setValue] = useState(0);
+  const { company, position, date, description } = jobs[value];
+  return (
+    <section className="section jobs">
+      <Title title="Experiência" />
+      <div className="jobs-center">
+        <div className="btn-container">
+          {jobs.map((job, index) => {
+            return (
+              <button
+                key={job.strapiId}
+                onClick={() => setValue(index)}
+                className={`job-btn ${index === value && 'active-btn'}`}
+              >
+                {job.company}
+              </button>
+            );
+          })}
+        </div>
+        <article className="job-info">
+          <h3>{position}</h3>
+          <h4>{company}</h4>
+          <p className="job-date">{date}</p>
+          {description.map(item => (
+            <div key={item.id} className="job-desc">
+              <FaAngleDoubleRight className="job-icon" />
+              <p>{item.name}</p>
+            </div>
+          ))}
+        </article>
+      </div>
+      <Link to="/about" className="btn center-btn">
+        Saber mais
+      </Link>
+    </section>
+  );
+};
 
-export default Jobs
+export default Jobs;
