fb05505b16b3c26d0c42f0116f92a0a3a11f5ba1
Bruno Mariano <bsignx@gmail.com>
2020-09-04 10:42:30 -0300

    refactor: sidebar animation

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -9,8 +9,8 @@ const Sidebar = ({ isOpen, handleToggleIsOpen }) => {
         <FaTimes />
       </button>
       <div className="side-container">
-        <Links styleClass="sidebar-links" />
-        <SocialLinks styleClass="sidebar-icons" />
+        <Links styleClass={`${isOpen ? 'sidebar-links' : ''}`} />
+        <SocialLinks styleClass={`${isOpen ? 'sidebar-icons' : ''}`} />
       </div>
     </aside>
   );
